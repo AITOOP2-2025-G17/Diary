78211d7f1460215c00500e08c9b6da4e49341c29
taku3mi <hiitan.r3.taku15@icloud.com>
2025-10-16 15:15:26 +0900

    Diary追加

---
+++ diaries/takuDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class DiarySample(AbstractDiary):
+    def get_date(self):
+        return "2025-10-16"
+
+    def get_summary(self):
+        return "gitの講義"
+
+    def get_author(self):
+        return "nagatani"
