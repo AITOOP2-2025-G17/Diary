962bc11920c43aebb21b5ef564c9b200e984e688
papettosunsun <8zz69ks6j8@privaterelay.appleid.com>
2025-10-16 15:49:30 +0900

    追加しました

---
+++ diaries/sunsunDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+
+class sunsunDiary(AbstractDiary):
+    def get_date(self):
+        return "2025-10-16"
+
+    def get_summary(self):
+        return "情報量多すぎてわからん！！！！"
+
+    def get_author(self):
+        return "takeichi"
